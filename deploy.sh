3610f2158f0b4dc9bb1b51aa981d964fef19d62e
落小梅 <425605679@qq.com>
2021-10-25 12:17:12 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ yarn build
 pwd
 zip -q -r -o public.zip public
 scp -i '/Users/jiabinbin/.ssh/root' public.zip root@163.53.219.60:/root/website/blog/
-ssh -i '/Users/jiabinbin/.ssh/root' root@163.53.219.60 'cd /root/website/blog/ ; ./deploy.sh'
+#ssh -i '/Users/jiabinbin/.ssh/root' root@163.53.219.60 'cd /root/website/blog/ ; ./deploy.sh'
 rm -rf public.zip
-git status
-git add .
-git status
-git commit -m 'update blog'
-git push origin master
-echo 'task finished'
-#
+#git status
+#git add .
+#git status
+#git commit -m 'update blog'
+#git push origin master
+#echo 'task finished'
+##
--- source/about/index.md
+++ source/about/index.md
@@ -4,7 +4,6 @@ date: 2017-10-12 10:48:25
 type: "about"
 updated: 2018-01-30 15:10:32
 aplayer: true
-
 ---
 
 > //todo (^_^)
