619b36112d5c21362179be1f578960d71c5b62de
落小梅 <425605679@qq.com>
2021-10-25 18:46:48 +0800

    update blog

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-rm -rf public/
-yarn build
-pwd
-zip -q -r -o public.zip public
-scp -i '/Users/jiabinbin/.ssh/root' public.zip root@163.53.219.60:/root/website/blog/
-ssh -i '/Users/jiabinbin/.ssh/root' root@163.53.219.60 'cd /root/website/blog/ ; ./deploy.sh'
-rm -rf public.zip
-git status
-git add .
-git status
-git commit -m 'update blog'
-git push origin mian
+rm -rf public/ &&
+yarn build &&
+pwd &&
+zip -q -r -o public.zip public &&
+scp -i '/Users/jiabinbin/.ssh/root' public.zip root@163.53.219.60:/root/website/blog/ &&
+ssh -i '/Users/jiabinbin/.ssh/root' root@163.53.219.60 'cd /root/website/blog/ ; ./deploy.sh' &&
+rm -rf public.zip &&
+git status &&
+git add . &&
+git status &&
+git commit -m 'update blog' &&
+git push origin mian &&
 echo 'task finished'
 ##
